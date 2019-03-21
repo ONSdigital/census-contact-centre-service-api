ed10a9fc4091c125c5075fc45ba9a0103ecb234d
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-03-21 16:51:35 +0000

    CR65 Switching response json from kebab to camel case field names

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressDTO.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressDTO.java
@@ -1,18 +1,15 @@
 package uk.gov.ons.ctp.integration.contactcentresvc.representation;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
-@JsonPropertyOrder({"uprn", "formatted-address", "welsh-formatted-address"})
+@JsonPropertyOrder({"uprn", "formattedAddress", "welshFormattedAddress"})
 public class AddressDTO {
 
   private String uprn;
 
-  @JsonProperty("formatted-address")
   private String formattedAddress;
 
-  @JsonProperty("welsh-formatted-address")
   private String welshFormattedAddress;
 }
--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressQueryResponseDTO.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/AddressQueryResponseDTO.java
@@ -1,16 +1,14 @@
 package uk.gov.ons.ctp.integration.contactcentresvc.representation;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 /** This response object is used to return a list of addresses for postcode and address queries. */
 @Data
-@JsonPropertyOrder({"data-version", "addresses", "total"})
+@JsonPropertyOrder({"dataVersion", "addresses", "total"})
 public class AddressQueryResponseDTO {
 
-  @JsonProperty("data-version")
   private String dataVersion;
 
   private ArrayList<AddressDTO> addresses;
