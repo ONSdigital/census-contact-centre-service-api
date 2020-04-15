43f9bcce2d3479f182c660eecfc1596ab02c2610
Phil Whiles <phil.whiles@gmail.com>
2020-04-15 10:57:58 +0100

    CR-000 SPLIT is now SPLIT_ADDRESS

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/CaseStatus.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/CaseStatus.java
@@ -2,6 +2,8 @@ package uk.gov.ons.ctp.integration.contactcentresvc.representation;
 
 public enum CaseStatus {
   UNCHANGED,
+  SPLIT_ADDRESS,
+  MERGED,
   DERELICT,
   DEMOLISHED,
   NON_RESIDENTIAL,
