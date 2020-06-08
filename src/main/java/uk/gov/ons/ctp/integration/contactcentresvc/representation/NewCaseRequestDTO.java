1753aef88dad5c2312b3ec80a6d1057bd3812d33
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-06-08 12:43:50 +0100

    CR-957 Updated objects to support the creation of new cases for new addresses

--- src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/NewCaseRequestDTO.java
+++ src/main/java/uk/gov/ons/ctp/integration/contactcentresvc/representation/NewCaseRequestDTO.java
@@ -2,11 +2,11 @@ package uk.gov.ons.ctp.integration.contactcentresvc.representation;
 
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import uk.gov.ons.ctp.common.domain.CaseType;
+import uk.gov.ons.ctp.common.domain.EstabType;
 
 /**
  * The request object when contact centre sends new case and address details
@@ -15,10 +15,13 @@ import uk.gov.ons.ctp.common.domain.CaseType;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewCaseRequestDTO extends CaseRequestDTO {
 
   @NotNull private CaseType caseType;
+
+  private String uprn;
+
+  @NotNull private EstabType estabType;
 }
